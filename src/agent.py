6b97c8645f95b125ee5e6d97ea550e7583634bb9
Haakam Aujla <haakamsingh21@gmail.com>
2025-06-25 22:44:53 -0700

    Context7

--- src/agent.py
+++ src/agent.py
@@ -1,25 +1,19 @@
 from typing import AsyncGenerator
 
-from agents import Agent, RawResponsesStreamEvent, Runner, function_tool
+from agents import Agent, RawResponsesStreamEvent, Runner
 from blaxel.openai import bl_model, bl_tools
 from openai.types.responses import ResponseTextDeltaEvent
 
 
-@function_tool()
-async def weather(city: str) -> str:
-    """Get the weather in a given city"""
-    return f"The weather in {city} is sunny"
-
-
 async def agent(input: str) -> AsyncGenerator[str, None]:
-    tools = await bl_tools(["blaxel-search"]) + [weather]
+    tools = await bl_tools(["context7"])
     model = await bl_model("sandbox-openai")
 
     agent = Agent(
-        name="blaxel-agent",
+        name="docs-agent",
         model=model,
         tools=tools,
-        instructions="You are a helpful assistant.",
+        instructions="You are a helpful developer assistant. You can answer questions about any documentation. Use context7 to get the relevant documentation and answer the user's query.",
     )
     result = Runner.run_streamed(agent, input)
     async for event in result.stream_events():
