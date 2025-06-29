66ba1f8d7dd9018cadd133173516131617afe965
Haakam Aujla <haakamsingh21@gmail.com>
2025-06-29 14:12:45 -0700

    improve agent

--- src/agent.py
+++ src/agent.py
@@ -6,7 +6,11 @@ from markdown import markdown
 
 
 async def agent(message: Message):
-    tools = await bl_tools(["context7"])
+    tools = [
+        tool
+        for tool in await bl_tools(["agentmail", "context7"])
+        if tool in ["get_thread", "resolve-library-id", "get-library-docs"]
+    ]
     model = await bl_model("gpt-4o-mini")
 
     agent = Agent(
@@ -18,11 +22,13 @@ You will receive an email from a developer.
 Your task is respond to the email in a friendly and helpful manner.
 
 If the developer asks a technical question, use the context7 tools to retieve the relevant documentation.
-First use the context7 resolve-library-id tool to resolve the relevant library id.
-Then use the context7 get-library-docs tool to retrieve the relevant documentation.
+First use the resolve-library-id tool to resolve the relevant library id.
+Then use the get-library-docs tool to retrieve the relevant documentation.
 
 Finally, respond to the developer as if you are writing an email.
 In the email signature, refer to yourself as "AgentMail".
+
+If the context of previous messages would be helpful for answering the developer's query, use the get_thread tool to retrieve the full email thread.
 
 Generate markdown and include relevant hyperlinks to the documentation.
 Do not specify the email subject, only the email body.
